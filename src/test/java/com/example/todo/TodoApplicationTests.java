822991a3e27339c8eeea97527c51ce22219a0ba3
AbhishekGMV <abhishek.mvg@gmail.com>
2022-03-13 17:49:31 +0530

    Test cases with mockito

--- src/test/java/com/example/todo/TodoApplicationTests.java
+++ src/test/java/com/example/todo/TodoApplicationTests.java
@@ -3,29 +3,57 @@ package com.example.todo;
 import com.example.todo.model.Todo;
 import com.example.todo.repo.TodoRepo;
 import com.example.todo.service.TodoServiceImpl;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
-class GradledemoApplicationTests {
+import java.util.ArrayList;
+import java.util.List;
+
+@ExtendWith(MockitoExtension.class)
+class TodoApplicationTests {
 
     @Mock
-    TodoRepo repo;
+    TodoRepo todoRepo;
 
     @InjectMocks
     TodoServiceImpl todoService;
 
     @Test
     public void testAddTodo() {
-        Todo todo = new Todo(1, "EAT", false);
-        Mockito.when(repo.save(todo)).thenReturn(todo);
+        Todo todo = new Todo(1,"EAT", true);
+        when(todoRepo.save(Mockito.any(Todo.class))).thenReturn(todo);
 
-        Todo res = todoService.addTask(todo);
-        Assertions.assertEquals(res.getTaskId(), todo.getTaskId());
+        ResponseEntity<?> res = todoService.addTask(todo);
+        HttpStatus expected = HttpStatus.OK;
+        HttpStatus actual = res.getStatusCode();
+        Assertions.assertEquals(expected, actual);
     }
 
+    @Test
+    public void testGetTodoList() {
+        Todo t1 = new Todo(1,"walk",false);
+        Todo t2 = new Todo(2,"sleep",false);
+        Todo t3 = new Todo(3,"code",true);
+        Todo t4 = new Todo(4,"run",false);
+
+        List<Todo> todoList = new ArrayList<>();
+        todoList.add(t1);
+        todoList.add(t2);
+        todoList.add(t3);
+        todoList.add(t4);
+
+        when(todoRepo.findAll()).thenReturn(todoList); //Stub -> if I call method x, return y
+        assertEquals(ResponseEntity.status(HttpStatus.OK).body(todoList), todoService.getTodoList()); //getTodoList method depends on mock findAll method
+        verify(todoRepo).findAll(); //Checks if findAll method was called exactly once
+    }
 }
