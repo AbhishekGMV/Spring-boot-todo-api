eb0b894b093f11809523921f25fa35496e977c84
Abhishek gowda M V <abhishek.mvg@gmail.com>
2022-03-15 17:50:04 +0530

    Swagger API docs integration

--- src/main/java/com/example/todo/TodoApplication.java
+++ src/main/java/com/example/todo/TodoApplication.java
@@ -3,6 +3,7 @@ package com.example.todo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//Swagger docs at /swagger-ui/index.html
 @SpringBootApplication
 public class TodoApplication {
     public static void main(String[] args) {
