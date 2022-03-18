452139ebf1d8d4fc5450a23af10e3294d4c4d45a
Abhishek gowda M V <abhishek.mvg@gmail.com>
2022-03-18 11:15:03 +0530

    API design changes

--- src/main/java/com/example/todo/controller/TodoController.java
+++ src/main/java/com/example/todo/controller/TodoController.java
@@ -5,34 +5,45 @@ import com.example.todo.service.TodoServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.view.RedirectView;
 
 @RestController
-@RequestMapping("/todo")
+@RequestMapping("/")
 public class TodoController {
     @Autowired
     TodoServiceImpl todoService;
 
-    @GetMapping("")
+    @GetMapping
+    public RedirectView redirectToSwaggerUI(){
+        return new RedirectView("swagger-ui/index.html");
+    }
+
+    @GetMapping("/todo")
+    public String rootController(){
+        return "Todo application API";
+    }
+
+    @GetMapping("/todo/all")
     public ResponseEntity<?> getTodoList() {
         return todoService.getTodoList();
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/todo/{id}")
     public ResponseEntity<?> getTodo(@PathVariable String id) {
         return todoService.getTodo(id);
     }
 
-    @PostMapping("/add")
+    @PostMapping("/todo/add")
     public ResponseEntity<?> addTask(@RequestBody Todo task) {
         return todoService.addTask(task);
     }
 
-    @DeleteMapping("/delete/{id}")
+    @DeleteMapping("/todo/delete/{id}")
     public ResponseEntity<?> deleteTask(@PathVariable int id) {
         return todoService.deleteTask(id);
     }
 
-    @PutMapping("/update")
+    @PutMapping("/todo/update")
     public ResponseEntity<?> updateTask(@RequestBody Todo task) {
         return todoService.updateTask(task);
     }
